d3a9e8234df9f99fbbdd67cb0e1f15495ff5ab1b
Gilles de Ghellinck <gillesdg95@gmail.com>
2018-11-07 10:32:31 +0100

    added average rating by user as well as fixed datetime

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,17 @@ class UsersController < ApplicationController
   end
 
   def show
+    total = []
+    pets = @user.pets.each do |pet|
+      pet.reviews.each do |review|
+        total << review.rating
+      end
+    end
+    if total.size == 0
+      @average = 0
+    else
+      @average = total.sum / total.size
+    end
   end
 
   def edit
