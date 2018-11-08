20086a8b316e086a686e93df7f4a40ce9b4227cb
Gilles de Ghellinck <gillesdg95@gmail.com>
2018-11-08 16:56:42 +0100

    added total price

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -34,6 +34,11 @@ class ContractsController < ApplicationController
 
   private
 
+  def price(pet)
+    length = self.start_date - self.end_date
+    return pet.price * length
+  end
+
   def set_contract_and_pet
     @pet    = Pet.find(params[:pet_id])
     @contract = Contract.find(params[:id])
